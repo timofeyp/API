3f465b512181695c91e9ec18dd0819db55d74714
timofeyp <timofeyp@live.com>
2018-11-23 10:18:16 +0300

    ADD mongodb cfg

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,6 +1,6 @@
 module.exports = function(app, db) {
     app.post('/notes', (req, res) => {
-        // Здесь будем создавать заметку.
+        console.log(req.body)
         res.send('Hello')
     });
 };
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
+const db             = require('./config/db');
 const app            = express();
-require('./routes')(app, {});
 const port = 8000;
-const server = app.listen(port, () => {
-    console.log('We are live on ' + port);
-});
-
-module.exports = server;
+app.use(bodyParser.urlencoded({ extended: true }));
+MongoClient.connect(db.url, { useNewUrlParser: true }, (err, database) => {
+    if (err) return console.log(err)
+    require('./app/routes')(app, database);
+    app.listen(port, () => {
+        console.log('We are live on ' + port);
+    });
+})
