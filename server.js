abb4b494a5babe1302a2c7147b3b88bd80565044
timofeyp <timofeyp@live.com>
2018-12-22 21:17:49 +0300

    ADD MORGAN REQUESTS LOG

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const mongoose = require('./app/database')
 const bot = require('./app/bot')
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
 const port = 8090
 app.use(bodyParser.urlencoded({ extended: true })) // ДОБАВЛЯЕТ В HTTP ЗАПРОС ОБЪЕКТ
 // BODY
@@ -12,3 +13,5 @@ app.listen(port, () => {
   console.log('We are live on ' + port)
   module.exports = mongoose
 })
+
+app.use(morgan('dev'))
